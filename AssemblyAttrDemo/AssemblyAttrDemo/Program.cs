7af3aa15efc7149723cc92326439c70570d8dc88
Ba Matrai <matrai.barnabas1@gmail.com>
2019-12-02 18:01:32 +0100

    finish exercise 2

---
+++ AssemblyAttrDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type type = typeof(AssemblyDescriptionAttribute);
+            object[] objects = assembly.GetCustomAttributes(type, false);
+
+            if (objects.Length > 0)
+            {
+                AssemblyDescriptionAttribute description = (AssemblyDescriptionAttribute) objects[0];
+
+                Console.WriteLine(description.Description);
+                Console.ReadKey();
+            }
+        }
+    }
+}
