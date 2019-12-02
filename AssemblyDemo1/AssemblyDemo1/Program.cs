070097c2bbdab7af50b2c2ab0fb9a5d6ca5ba6f8
Ba Matrai <matrai.barnabas1@gmail.com>
2019-12-02 21:33:08 +0100

    finish exercise 3

---
+++ AssemblyDemo1/AssemblyDemo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.ServiceProcess.dll";
+            BindingFlags bindingFlags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+
+            Assembly assembly = Assembly.LoadFrom(path);
+            Console.WriteLine(assembly.FullName);
+
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                Console.WriteLine(type.Name);
+                MemberInfo[] members = type.GetMembers(bindingFlags);
+
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(member.MemberType);
+                    Console.WriteLine(member.Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
